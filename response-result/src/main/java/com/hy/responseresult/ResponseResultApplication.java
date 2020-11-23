516b1abfd5de6eb829bf3594dca1b224ba030b39
yang ran <yangran@xytdt.com>
2020-11-23 21:11:58 +0800

    升级swagger皮肤

--- response-result/src/main/java/com/hy/responseresult/ResponseResultApplication.java
+++ response-result/src/main/java/com/hy/responseresult/ResponseResultApplication.java
@@ -2,9 +2,7 @@ package com.hy.responseresult;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
-@EnableOpenApi
 @SpringBootApplication
 public class ResponseResultApplication {
 
