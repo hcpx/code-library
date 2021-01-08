56d4b80127fd8c1a4ab5717a5ead2233ecccabb0
tangjian <tangjian@yzw.cn>
2021-01-08 11:44:32 +0800

    修复total能被batchSize整除时，循环次数多了一次的bug

--- async-progress-info/src/main/java/com/hy/async/async/AsyncManagerService.java
+++ async-progress-info/src/main/java/com/hy/async/async/AsyncManagerService.java
@@ -52,9 +52,9 @@ public class AsyncManagerService {
                 int start;
                 int end;
 
-                int count = total / batchSize;
+                int count = total % batchSize == 0 ? total / batchSize : total / batchSize + 1;
 
-                for (int i = 0; i <= count; i++) {
+                for (int i = 0; i < count; i++) {
                     start = i * batchSize;
                     end = start + batchSize;
 
